07ce40c48b4a5a958da6366c5eeae93811a3814a
Sandra Greenhalgh <sandragreenhalgh@gmail.com>
2023-07-30 18:04:13 +0800

    linting

--- apps/my-app/src/components/TheWelcome.vue
+++ apps/my-app/src/components/TheWelcome.vue
@@ -15,7 +15,7 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #heading>Documentation</template>
 
     Vue's
-    <a href="https://vuejs.org/" target="_blank" rel="noopener"> official documentation </a>
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
     provides you with all information you need to get started.
   </WelcomeItem>
 
@@ -26,16 +26,15 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #heading>Tooling</template>
 
     This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener"> Vite </a>
-    . The recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener"> VSCode </a>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The recommended IDE setup
+    is
+    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a>
     +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener"> Volar </a>
-    . If you need to test your components and web pages, check out
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If you need to test your
+    components and web pages, check out
     <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a>
     and
-    <a href="https://on.cypress.io/component" target="_blank"> Cypress Component Testing </a>
-    .
+    <a href="https://on.cypress.io/component" target="_blank">Cypress Component Testing</a>.
 
     <br />
 
@@ -67,11 +66,10 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #heading>Community</template>
 
     Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>
-    , our official Discord server, or
+    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official Discord server, or
     <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener">StackOverflow</a>
     . You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener"> our mailing list </a>
+    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a>
     and follow the official
     <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
     twitter account for latest news in the Vue world.
