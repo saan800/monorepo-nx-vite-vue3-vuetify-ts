a61e9d5f82b0ab1325e49838660adca1111fa725
Sandra Greenhalgh <sandragreenhalgh@gmail.com>
2023-08-10 19:27:01 +0800

    pinia store

--- apps/my-app/src/main.ts
+++ apps/my-app/src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from '@/App.vue'
 import vuetify from '@/plugins/vuetify'
+import store from '@/store'
 
 const app = createApp(App)
+app.use(store)
 app.use(vuetify)
 
 app.mount('#app')
