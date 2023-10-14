1df89373c9168ac5d6a4b61a263a2dae919c0394
Sandra Greenhalgh <sandragreenhalgh@gmail.com>
2023-10-15 01:31:20 +0800

    get tests working

--- apps/my-app/src/components/HelloWorld.test.ts
+++ apps/my-app/src/components/HelloWorld.test.ts
@@ -1,10 +1,17 @@
 import { mount } from '@vue/test-utils'
 import { describe, expect, it } from 'vitest'
+
 import HelloWorld from '@/components/HelloWorld.vue'
+import vuetify from '@/plugins/vuetify'
 
 describe('HelloWorld', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
+    const wrapper = mount(HelloWorld, {
+      global: {
+        plugins: [vuetify]
+      },
+      props: { msg: 'Hello Vitest' }
+    })
     expect(wrapper.text()).toContain('Hello Vitest')
   })
 })
