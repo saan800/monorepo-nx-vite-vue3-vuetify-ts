e80675328bd5723a2a4a1e693f0248afda606158
Sandra Greenhalgh <sandragreenhalgh@gmail.com>
2023-10-15 01:41:46 +0800

    tests

--- apps/my-app/src/components/HelloWorld.test.ts
+++ apps/my-app/src/components/HelloWorld.test.ts
@@ -1,8 +1,11 @@
 import { mount } from '@vue/test-utils'
 import { describe, expect, it } from 'vitest'
+import { createVuetify } from 'vuetify'
 
 import HelloWorld from '@/components/HelloWorld.vue'
-import vuetify from '@/plugins/vuetify'
+//import vuetify from '@/plugins/vuetify'
+
+const vuetify = createVuetify({})
 
 describe('HelloWorld', () => {
   it('renders properly', () => {
