68cf3eeadeaa3857be72f7ea3e8484ba1d81bb91
Sandra Greenhalgh <sandragreenhalgh@gmail.com>
2023-10-17 01:20:33 +0800

    vuetify

--- apps/my-app/src/plugins/vuetify.ts
+++ apps/my-app/src/plugins/vuetify.ts
@@ -1,22 +1,37 @@
 /**
- * plugins/vuetify.ts
- *
  * Framework documentation: https://vuetifyjs.com`
  */
-
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
+import { createVuetify, type VuetifyOptions } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
+// Translations provided by Vuetify
+import { en } from 'vuetify/locale'
+
 import { loadFonts } from '@/plugins/webfontloader'
+import '@/styles/main.scss'
 
 await loadFonts()
 
-// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-export default createVuetify({
-  components,
-  directives,
+let vuetifyConfig: VuetifyOptions = {
+  // Icon Fonts
+  // https://vuetifyjs.com/en/features/icon-fonts/
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi
+    }
+  },
+  // Internationalization (i18n)
+  // https://vuetifyjs.com/en/features/internationalization/#internationalization-i18n
+  locale: {
+    locale: 'en',
+    fallback: 'en',
+    messages: { en }
+  },
+  // Theme
+  // https://vuetifyjs.com/en/features/theme/
   theme: {
     themes: {
       light: {
@@ -26,14 +41,19 @@ export default createVuetify({
         }
       }
     }
-  },
-  // Icon Fonts
-  // https://vuetifyjs.com/en/features/icon-fonts/
-  icons: {
-    defaultSet: 'mdi',
-    aliases,
-    sets: {
-      mdi
-    }
   }
-})
+}
+
+if (import.meta.env.DEV) {
+  // Disable tree shaking for DEV mode.
+  vuetifyConfig = {
+    components,
+    directives,
+    ...vuetifyConfig
+  }
+}
+
+export default createVuetify(vuetifyConfig)
+
+// Export for test.
+export { components, directives }
